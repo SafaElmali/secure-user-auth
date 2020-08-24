28059e65dac3516ac2ec7008c4a32884d618fda9
Safa Elmali <tsafaelmali@gmail.com>
2020-08-24 16:02:18 +0300

    navigation conditionally

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from "react-router-dom";
+import { AuthContext } from '../../context/AutContext';
 
 const Home = () => {
+    const authContext = useContext(AuthContext);
+
     return (
         <nav>
             <ul>
                 <li>
-                    <Link to="/login">Login</Link>
+                    <Link to={authContext.isAuthenticated() ? '/dashboard' : 'login'}>Login</Link>
                 </li>
                 <li>
-                    <Link to="/Signup">Signup</Link>
+                    <Link to='/signup'>Signup</Link>
+                </li>
+                <li>
+                    <Link to={authContext.isAuthenticated() ? '/dashboard' : 'signup'}>Get Started</Link>
                 </li>
             </ul>
         </nav>
