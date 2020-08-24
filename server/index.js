d52dfe9fefdb5e8fb7a19d7d56f2a6fb27679ca0
Safa Elmali <tsafaelmali@gmail.com>
2020-08-24 15:27:17 +0300

    removed context

--- server/index.js
+++ server/index.js
@@ -18,32 +18,32 @@ mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTop
 const server = new ApolloServer({
     typeDefs,
     resolvers,
-    context: ({ req }) => {
-        // Note! This example uses the `req` object to access headers,
-        // but the arguments received by `context` vary by integration.
-        // This means they will vary for Express, Koa, Lambda, etc.!
-        //
-        // To find out the correct arguments for a specific integration,
-        // see the `context` option in the API reference for `apollo-server`:
-        // https://www.apollographql.com/docs/apollo-server/api/apollo-server/
+    // context: ({ req }) => {
+    //     // Note! This example uses the `req` object to access headers,
+    //     // but the arguments received by `context` vary by integration.
+    //     // This means they will vary for Express, Koa, Lambda, etc.!
+    //     //
+    //     // To find out the correct arguments for a specific integration,
+    //     // see the `context` option in the API reference for `apollo-server`:
+    //     // https://www.apollographql.com/docs/apollo-server/api/apollo-server/
 
-        // Get the user token from the headers.
-        context: async ({ req }) => {
-            const auth = req ? req.headers.authorization : null
-            if (auth && auth.toLowerCase().startsWith('bearer ')) {
-                const decodedToken = jwt.verify(
-                    auth.substring(7), process.env.JWT_SECRET)
+    //     // Get the user token from the headers.
+    //     context: async ({ req }) => {
+    //         const auth = req ? req.headers.authorization : null
+    //         if (auth && auth.toLowerCase().startsWith('bearer ')) {
+    //             const decodedToken = jwt.verify(
+    //                 auth.substring(7), process.env.JWT_SECRET)
 
-                // we could also check user roles/permissions here
-                // optionally block the user
-                if (!decodedToken) throw new AuthenticationError('you must be logged in');
+    //             // we could also check user roles/permissions here
+    //             // optionally block the user
+    //             if (!decodedToken) throw new AuthenticationError('you must be logged in');
 
-                const currentUser = await User.findById(decodedToken.id)
+    //             const currentUser = await User.findById(decodedToken.id)
 
-                return { currentUser }
-            }
-        }
-    }
+    //             return { currentUser }
+    //         }
+    //     }
+    // }
 });
 
 server.listen().then(({ url }) => {
