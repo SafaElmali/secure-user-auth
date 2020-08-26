965acec6f12aa2ba4b3040197c69eee36cd11bbc
Safa Elmali <tsafaelmali@gmail.com>
2020-08-26 12:30:54 +0300

    added client side  request jwt config

--- src/index.js
+++ src/index.js
@@ -2,13 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
+import { setContext } from 'apollo-link-context';
+
+
+// authorization config : https://www.apollographql.com/docs/react/networking/authentication/#header 
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000/',
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    }
+  }
+});
 
 const client = new ApolloClient({
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache({
     addTypename: false
   }),
-  uri: "http://localhost:4000/"
 });
 
 ReactDOM.render(
